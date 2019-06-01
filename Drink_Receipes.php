6647a91285109f56123fa7f1e06dbef2e315b7c7
Donya <moxleydevelopmenta@gmail.com>
2019-06-01 01:18:43 -0400

    adding the class created for the drink receipes after they are read from a csv file

---
+++ Drink_Receipes.php
@@ -0,0 +1,431 @@
+<?php 
+
+class Cocktails {
+
+    /* Member variables */
+    private $strDrink;
+    private $strAlcoholic;
+    private $strCategory;
+    private $strDrinkThumb;
+    private $strGlass;
+    private $strIBA;
+    private $strIngredient1;
+    private $strIngredient10;
+    private $strIngredient11;
+    private $strIngredient12;
+    private $strIngredient2;
+    private $strIngredient3;
+    private $strIngredient4;
+    private $strIngredient5;
+    private $strIngredient6;
+    private $strIngredient7;
+    private $strIngredient8;
+    private $strIngredient9;
+    private $strInstructions;
+    private $strInstructions1;
+    private $strMeasure10;
+    private $strMeasure11;
+    private $strMeasure12;
+    private $strMeasure13;
+    private $strMeasure14;
+    private $strMeasure15;
+    private $strMeasure2;
+    private $strMeasure3;
+    private $strMeasure4;
+    private $strMeasure5;
+    private $strMeasure6;
+    private $strMeasure7;
+    private $strMeasure8;
+    private $strMeasure9;
+    /*
+
+    function _construct(array $cocktails) {
+
+      
+
+      $this->strDrink = $cocktails[0];
+      $this->strAlcohlic = $cocktails[1];
+      $this->strCategory = $cocktails[2];
+      $this->strDrinkThumb = $cocktails[3];
+      $this->strGlass = $cocktails[4];
+      $this->strIBA = $cocktails[5];
+      $this->strIngredient1 = $cocktails[6];
+      $this->strIngredient10 = $cocktails[7];
+      $this->strIngredient11 = $cocktails[8];
+      $this->strIngredient12 = $cocktails[9];
+      $this->strIngredient2 =$cocktails[10];
+      $this->strIngredient3 =$cocktails[11];
+      $this->strIngredient4 = $cocktails[12];
+      $this->strIngredient5 =$cocktails[13];
+      $this->strIngredient6 = $cocktails[14];
+      $this->strIngredient7 =$cocktails[15];
+      $this->strIngredient8 =  $cocktails[16];
+      $this->strIngredient9 =  $cocktails[17];
+      $this->strInstructions =$cocktails[18];
+      $this->strInstructions1 =$cocktails[19];
+      $this->strMeasure10 = $cocktails[20];
+      $this->strMeasure11 =  $cocktails[21];
+      $this->strMeasure12 =  $cocktails[22];
+      $this->strMeasure13 =  $cocktails[23];
+      $this->strMeasure14 = $cocktails[24];
+      $this->strMeasure15 =  $cocktails[25];
+      $this->strMeasure2 =  $cocktails[26];
+      $this->strMeasure3 =  $cocktails[27];
+      $this->strMeasure4 = $cocktails[28];
+      $this->strMeasure5 = $cocktails[29];
+      $this->strMeasure6 =  $cocktails[30];
+      $this->strMeasure7 =  $cocktails[31];
+      $this->strMeasure8 =  $cocktails[32];
+      $this->strMeasure9 = $cocktails[33];
+
+    }
+
+    */
+  
+
+    /* Member functions */
+    function setDrink($par){
+        $this->strDrink = $par;
+     }
+     
+     function getDrink(){
+        echo $this->strDrink;
+     }
+
+     function setAlcohlic($par){
+        $this->strAlcohlic = $par;
+     }
+     
+     function getAlcohlic(){
+        echo $this->strAlcohlic;
+     }
+
+     function setCategory($par){
+       $this->strCategory = $par;
+     }
+     
+     function getCategory(){
+        echo $this->strCategory;
+     }
+
+     function setDrinkThumb($par){
+        $this->strDrinkThumb = $par;
+     }
+     
+     function getDrinkThumb(){
+        echo $this->strDrinkThumb;
+     }
+
+     function setGlass($par){
+        $this->strGlass = $par;
+     }
+     
+     function getGlass(){
+        echo $this->strGlass;
+     }
+
+     function setIBA($par){
+        $this->strIBA = $par;
+     }
+     
+     function getIBA(){
+        echo $this->strIBA;
+     }
+
+     function setIngredient1($par){
+        $this->strIngredient1 = $par;
+     }
+     
+     function getIngredient1(){
+        echo $this->strIngredient1;
+     }
+
+     function setIngredient10($par){
+      $this->strIngredient10 = $par;
+   }
+   
+   function getIngredient10(){
+      echo $this->strIngredient10;
+   }
+
+     function setIngredient11($par){
+        $this->strIngredient11 = $par;
+     }
+     
+     function getIngredient11(){
+        echo $this->strIngredient11;
+     }
+
+     function setIngredient12($par){
+        $this->strIngredient12 = $par;
+     }
+     
+     function getIngredient12(){
+        echo $this->strIngredient12;
+     }
+
+     function setIngredient2($par){
+        $this->strIngredient2 = $par;
+     }
+     
+     function getIngredient2(){
+        echo $this->strIngredient2;
+     }
+
+     function setIngredient3($par){
+        $this->strIngredient3 = $par;
+     }
+     
+     function getIngredient3(){
+        echo $this->strIngredient3;
+     }
+
+     function setIngredient4($par){
+        $this->strIngredient4 = $par;
+     }
+     
+     function getIngredient4(){
+        echo $this->strIngredient4;
+     }
+
+     function setIngredient5($par){
+        $this->strIngredient5= $par;
+     }
+     
+     function getIngredient5(){
+        echo $this->strIngredient5;
+     }
+
+     function setIngredient6($par){
+        $this->strIngredient6 = $par;
+     }
+     
+     function getIngredient6(){
+        echo $this->strIngredient6;
+     }
+
+     function setIngredient7($par){
+        $this->strIngredient7 = $par;
+     }
+     
+     function getIngredient7(){
+        echo $this->strIngredient7;
+     }
+
+     function setIngredient8($par){
+        $this->strIngredient8 = $par;
+     }
+     
+     function getIngredient8(){
+        echo $this->strIngredient8;
+     }
+
+     function setIngredient9($par){
+        $this->strIngredient9 = $par;
+     }
+     
+     function getIngredient9(){
+        echo $this->strIngredient9;
+     }
+
+     function setInstructions($par){
+        $this->strInstructions = $par;
+     }
+     
+     function getInstructions(){
+        echo $this->strInstructions;
+     }
+
+     function setInstructions1($par){
+        $this->strInstructions1 = $par;
+     }
+     
+     function getInstructions1(){
+        echo $this->strInstructions1;
+     }
+
+     function setMeasure10($par){
+        $this->strMeasure10 = $par;
+     }
+     
+     function getMeasure10(){
+        echo $this->strMeasure10;
+     }
+
+     function setMeasure11($par){
+        $this->strMeasure11 = $par;
+     }
+     
+     function getMeasure11(){
+        echo $this->strMeasure11;
+     }
+
+     function setMeasure12($par){
+        $this->strMeasure12 = $par;
+     }
+     
+     function getMeasure12(){
+        echo $this->strMeasure12;
+     }
+
+     function setMeasure13($par){
+        $this->strMeasure13 = $par;
+     }
+     
+     function getMeasure13(){
+        echo $this->strMeasure13;
+     }
+
+     function setMeasure14($par){
+        $this->strMeasure14 = $par;
+     }
+     
+     function getMeasure14(){
+        echo $this->strMeasure14;
+     }
+
+     function setMeasure15($par){
+        $this->strMeasure15 = $par;
+     }
+     
+     function getMeasure15(){
+        echo $this->strMeasure15;
+     }
+
+     function setMeasure2($par){
+        $this->strMeasure2 = $par;
+     }
+     
+     function getMeasure2(){
+        echo $this->strMeasure2;
+     }
+
+     function setMeasure3($par){
+        $this->strMeasure3 = $par;
+     }
+     
+     function getMeasure3(){
+        echo $this->strMeasure3;
+     }
+
+     function setMeasure4($par){
+        $this->strMeasure4 = $par;
+     }
+     
+     function getMeasure4(){
+        echo $this->strMeasure4;
+     }
+
+     function setMeasure5($par){
+        $this->strMeasure5 = $par;
+     }
+     
+     function getMeasure5(){
+        echo $this->strMeasure5;
+     }
+
+     function setMeasure6($par){
+        $this->strMeasure6 = $par;
+     }
+     
+     function getMeasure6(){
+        echo $this->strMeasure6;
+     }
+
+     function setMeasure7($par){
+        $this->strMeasure7 = $par;
+     }
+     
+     function getMeasure7(){
+        echo $this->strMeasure7;
+     }
+
+     function setMeasure8($par){
+        $this->strMeasure8 = $par;
+     }
+     
+     function getMeasure8(){
+        echo $this->strMeasure8;
+     }
+
+     function setMeasure9($par){
+        $this->strMeasure9 = $par;
+     }
+     
+     function getMeasure9(){
+        echo $this->strMeasure9;
+     }
+
+}
+
+
+
+$file = fopen("cocktaildb.csv","r");
+while(! feof($file))
+  {
+     
+   $tester[] = (fgetcsv($file));
+    
+   
+  }
+ 
+  
+  $drinks = array();
+
+  for($x =1; $x < count($tester); $x++){
+
+    $drink = new Cocktails();
+  
+  
+  $drink->setDrink($tester[$x][0]);
+  $drink->setAlcohlic($tester[$x][1]);
+  $drink->setCategory($tester[$x][2]);
+  $drink->setDrinkThumb($tester[$x][3]);
+  $drink->setGlass($tester[$x][4]);
+  $drink->setIBA($tester[$x][5]);
+  $drink->setIngredient1($tester[$x][6]);
+  $drink->setIngredient10($tester[$x][7]);
+  $drink->setIngredient11($tester[$x][8]);
+  $drink->setIngredient12($tester[$x][9]);
+  $drink->setIngredient2($tester[$x][10]);
+  $drink->setIngredient3($tester[$x][11]);
+  $drink->setIngredient4($tester[$x][12]);
+  $drink->setIngredient5($tester[$x][13]);
+  $drink->setIngredient6($tester[$x][14]);
+  $drink->setIngredient7($tester[$x][15]);
+  $drink->setIngredient8($tester[$x][16]);
+  $drink->setIngredient9($tester[$x][17]);
+  $drink->setInstructions($tester[$x][18]);
+  $drink->setInstructions1($tester[$x][19]);
+  $drink->setMeasure10($tester[$x][20]);
+  $drink->setMeasure11($tester[$x][21]);
+  $drink->setMeasure12($tester[$x][22]);
+  $drink->setMeasure13($tester[$x][23]);
+  $drink->setMeasure14($tester[$x][24]);
+  $drink->setMeasure15($tester[$x][25]);
+  $drink->setMeasure2($tester[$x][26]);
+  $drink->setMeasure3($tester[$x][27]);
+  $drink->setMeasure4($tester[$x][28]);
+  $drink->setMeasure5($tester[$x][29]);
+  $drink->setMeasure6($tester[$x][30]);
+  $drink->setMeasure7($tester[$x][31]);
+  $drink->setMeasure8($tester[$x][32]);
+  $drink->setMeasure9($tester[$x][33]);
+
+  array_push($drinks, $drink);
+   
+  }
+  
+ 
+ 
+
+ 
+
+fclose($file);
+
+
+
+
+
+
+?>
